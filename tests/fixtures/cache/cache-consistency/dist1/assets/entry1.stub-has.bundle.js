784a901495aeff0b6aa1b294e0a1ec0874dfe478
Avinash-1994 <pavinash469@gmail.com>
2026-02-27 12:52:44 +0530

    chnages

---
+++ tests/fixtures/cache/cache-consistency/dist1/assets/entry1.stub-has.bundle.js
@@ -0,0 +1,8 @@
+(function(g){const m={};g.d=(i,f)=>m[i]=f;g.r=i=>{if(m[i].z)return m[i].z;const o={e:{}};m[i](o,o.e,g.r);return m[i].z=o.e}})(globalThis);
+
+globalThis.d("n0",function(module,exports,require,h){
+import { shared } from './shared';
+console.log('Entry1:', shared);
+});
+
+globalThis.r("n0");
